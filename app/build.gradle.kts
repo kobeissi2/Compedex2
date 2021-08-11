7f425e0f6b7ca55592d5225e74159a5cf1c56468
Moe <mohamadk@umich.edu>
2021-08-11 13:05:31 -0400

    Readded kotlinCompilerExtensionVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+val composeVersion = "1.1.0-alpha01"
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -37,6 +39,7 @@ android.run {
         targetCompatibility(JavaVersion.VERSION_1_8)
     }
 
+    composeOptions.kotlinCompilerExtensionVersion = composeVersion
     kotlinOptions.jvmTarget = "1.8"
     buildFeatures.compose = true
 }
@@ -53,19 +56,19 @@ dependencies {
     // Used for hiltViewModel() in Compose
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
     // Jetpack Compose
-    implementation("androidx.compose.ui:ui:1.1.0-alpha01")
+    implementation("androidx.compose.ui:ui:$composeVersion")
     // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.1.0-alpha01")
-    implementation("androidx.compose.runtime:runtime:1.1.0-alpha01")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.runtime:runtime:$composeVersion")
     // implementations support for setContent in activities
     implementation("androidx.activity:activity-compose:1.3.1")
     // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
-    implementation("androidx.compose.foundation:foundation:1.1.0-alpha01")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
     // Material Design
-    implementation("androidx.compose.material:material:1.1.0-alpha01")
+    implementation("androidx.compose.material:material:$composeVersion")
     // Material design icons
-    implementation("androidx.compose.material:material-icons-core:1.1.0-alpha01")
-    implementation("androidx.compose.material:material-icons-extended:1.1.0-alpha01")
+    implementation("androidx.compose.material:material-icons-core:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     // Accompanist to supplement Jetpack Compose
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.16.1")
     // Navigation in Compose
@@ -102,7 +105,7 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0-alpha01")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 
     kapt("com.google.dagger:hilt-android-compiler:2.38.1")
     kapt("androidx.room:room-compiler:2.4.0-alpha04")
